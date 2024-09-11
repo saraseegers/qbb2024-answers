13521e069410b338f3ad11fa24de32706ead8439
Sara Seegers <sseeger4@jh.edu>
2024-09-11 11:05:42 -0400

    gtfbed.py updated

--- day2-homework/gtf2bed.py
+++ day2-homework/gtf2bed.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
+#imports system
 import sys
-
-my_file = open( sys.argv[1] )
-
 
 #this line is getting file
 #sys provides communication bc command line and script
 #argv is a list of evyerthing that was typed in at the terminal  
+my_file = open( sys.argv[1] )
+
 
 for my_line in my_file:
     if "##" in my_line:
@@ -16,9 +16,9 @@ for my_line in my_file:
 
     s = fields[8]. split(";")[2].lstrip('gene_name "').rstrip('"')
 
-    print( fields [0], fields [3], fields [4], s)
+    #join fields with tab character for BED format 
+    print( fields [0], fields [3], fields [4], s, sep= '\t')
 
-    
-
+#close file     
 my_file.close()
 
