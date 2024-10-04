c0b99542960168511c29d86064692fb31e856d44
Sara Seegers <sseeger4@jh.edu>
2024-10-03 22:36:20 -0400

    shell script for ex2

---
+++ qblab_hw3/hw3_ex2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# #2.1
+
+# REF="/Users/cmdb/qbb2024-answers/qblab_hw3/sacCer3.fa"
+
+# # Number of threads (for faster processing)
+# THREADS=4
+
+# # Loop through each sample (base names without .fastq)
+# for SAMPLE in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
+# do
+#   # Single-end read file (adjust file paths as needed)
+#   READ="/Users/cmdb/qbb2024-answers/qblab_hw3/${SAMPLE}.fastq"  # Single FASTQ file for this sample
+
+#   # Output SAM file
+#   OUT_SAM="/Users/cmdb/qbb2024-answers/qblab_hw3/${SAMPLE}.aligned.sam"
+
+#   # Run the alignment using bwa mem for single-end data
+#   bwa mem -t $THREADS -R "@RG\tID:${SAMPLE}\tSM:${SAMPLE}" $REF $READ > $OUT_SAM
+
+# done
+
+
+#2.2
+
+# Reference genome file
+REF="/Users/cmdb/qbb2024-answers/qblab_hw3/sacCer3.fa"
+
+# Number of threads (for faster processing)
+THREADS=4
+
+# Loop through each sample (base names without .fastq)
+for SAMPLE in A01_09 A01_11 A01_23 A01_24 A01_27 A01_31 A01_35 A01_39 A01_62 A01_63
+do
+  # Single-end read file (adjust file paths as needed)
+  READ="/Users/cmdb/qbb2024-answers/qblab_hw3/${SAMPLE}.fastq"  # Single FASTQ file for this sample
+
+  # Output SAM file (temporary, will be replaced by sorted BAM)
+  OUT_SAM="/Users/cmdb/qbb2024-answers/qblab_hw3/${SAMPLE}.aligned.sam"
+
+  # Run the alignment using bwa mem
+  bwa mem -t $THREADS -R "@RG\tID:${SAMPLE}\tSM:${SAMPLE}" $REF $READ > $OUT_SAM
+
+  # Define sorted BAM file output
+  OUT_BAM="/Users/cmdb/qbb2024-answers/qblab_hw3/${SAMPLE}.sorted.bam"
+
+  # Sort the SAM file and convert it to BAM format
+  samtools sort -O bam -o $OUT_BAM $OUT_SAM
+
+  # Index the sorted BAM file
+  samtools index $OUT_BAM
+
+  # (Optional) Remove the intermediate SAM file to save space
+  # rm $OUT_SAM
+
+done
